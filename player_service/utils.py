54412c35bf3359351bd8dc67f0453130d3cb705d
José María <josemariacruzlorite@gmail.com>
2023-01-12 15:44:10 +0100

    Implement get_request_json_as_dict function

--- player_service/utils.py
+++ player_service/utils.py
@@ -7,3 +7,15 @@ from flask import request, abort
 # UTILS                                                                                            #
 ####################################################################################################
 
+def get_request_json_as_dict():
+    """Returns request as a JSON. Content-Type must be application/json, otherwise 400 error will be
+    thrown.
+    """
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        try:
+            return json.loads(request.data)
+        except Exception as exc:
+            abort(400, 'Request JSON data is ill-formed.')
+    else:
+        abort(400, 'Content-Type must be application/json.')

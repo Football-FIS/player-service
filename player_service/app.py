0e57fc053afdb08beb37c3f4fa14a4b9d23f5bbf
José María <josemariacruzlorite@gmail.com>
2023-01-12 15:31:30 +0100

    Implement /api/v1/players/ endpoint

--- player_service/app.py
+++ player_service/app.py
@@ -1,16 +1,39 @@
-#!/usr/bin/env python
-# encoding: utf-8
-
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, abort
+
+from flask_restful import Api
+from flask_pymongo import PyMongo
+
+from model import Player
 
 
 app = Flask(__name__)
 
+api = Api(app)
+
 # set secret key from environment variable
 app.secret_key = os.environ['SECRET_KEY']
 
-@app.route('/hello', methods=['GET'])
-def hello():
-    return "hello-from-flask"
+app.config["MONGO_URI"] = os.environ['MONGO_URL']
+mongo = PyMongo(app)
+
+####################################################################################################
+# ENDPOINTS                                                                                        #
+####################################################################################################
+
+@app.route('/api/v1/players/<string:team_id>', methods=['GET'])
+def get_players(team_id: str):
+    team = {
+        'team_id': team_id,
+        'players': [p for p in mongo.db.players.find({'team_id': team_id})]
+    }
+
+    if not team['players']:
+        abort(400, f'there are no players for team "{str(team_id)}"')
+
+    # cast id to strong
+    for player in team['players']:
+        player['_id'] = str(player['_id'])
+        
+    return jsonify(team)

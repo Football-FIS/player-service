211a2bfd56bb3d9f317d73a6a07d8bfeb7d22278
José María <josemariacruzlorite@gmail.com>
2023-01-19 14:44:29 +0100

    Create CRUD test and notify-players test

--- player_service/test_app.py
+++ player_service/test_app.py
@@ -1,12 +1,57 @@
 import os
 import requests
 
-import pytest
-
-
 # test token
 PLAYER_SERVICE = os.environ['PLAYER_SERVICE_URL']
-AUTH_TOKEN = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA1NjY0NjI1LCJpYXQiOjE2NzQxMjg2MjUsImp0aSI6IjQwZmNiN2IwZThjNjQwZDBiNzIwNzE3YmE0ZGE5YWQxIiwidXNlcl9pZCI6MTh9.JosnrKeXycoSFnj0CYE-429wDTJEGc8vkIrae8cPHy0"
+AUTH_TOKEN = os.environ['AUTH_TOKEN']
+
+# test data
+PLAYER = {
+    "_id": "",
+    "email": "jose@email.com",
+    "first_name": "jose",
+    "last_name": "gonzalez",
+    "position": "DELANTERO",
+    "team_id": 0
+}
+HEADERS = {'Authorization': AUTH_TOKEN, 'Content-Type': 'application/json'}
+
 
 def test_post():
-    return True
+    res = requests.post(PLAYER_SERVICE + '/api/v1/player', headers=HEADERS, json=PLAYER)
+    assert res.status_code == 200, res.text
+    global PLAYER_ID
+    PLAYER_ID = res.json()['_id']
+
+def test_get():
+    res = requests.get(PLAYER_SERVICE + f'/api/v1/player/{PLAYER_ID}', headers=HEADERS)
+    assert res.status_code == 200, PLAYER_ID
+
+def test_put():
+    res = requests.put(PLAYER_SERVICE + f'/api/v1/player/{PLAYER_ID}', headers=HEADERS, json=PLAYER)
+    assert res.status_code == 200, res.text
+
+def test_delete():
+    res = requests.delete(PLAYER_SERVICE + f'/api/v1/player/{PLAYER_ID}', headers=HEADERS)
+    assert res.status_code == 200, res.text
+
+def test_players():
+    res = requests.get(PLAYER_SERVICE + f'/api/v1/players/', headers=HEADERS)
+    assert res.status_code == 200, res.text
+
+def test_notify_players():
+    match = {
+        "alignment": "string",
+        "city": "string",
+        "id": "string",
+        "is_local": True,
+        "opponent": "string",
+        "sent_email": True,
+        "start_date": "string",
+        "url": "string",
+        "user_id": 0,
+        "weather": "string"
+    }
+
+    res = requests.post(PLAYER_SERVICE + f'/api/v1/notify-players/', headers=HEADERS, json=match)
+    assert res.status_code == 202, res.text
